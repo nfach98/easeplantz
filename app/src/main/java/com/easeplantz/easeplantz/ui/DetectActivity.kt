964bc01355c59e7b8a016b2b8e468db1f41b388f
nfach98 <nfach98@gmail.com>
2021-06-01 18:59:01 +0700

    add send data screen

--- app/src/main/java/com/easeplantz/easeplantz/ui/DetectActivity.kt
+++ app/src/main/java/com/easeplantz/easeplantz/ui/DetectActivity.kt
@@ -3,10 +3,17 @@ package com.easeplantz.easeplantz.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.easeplantz.easeplantz.R
+import com.easeplantz.easeplantz.databinding.ActivityDetectBinding
 
 class DetectActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetectBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detect)
+        binding = ActivityDetectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.home.setOnClickListener { finish() }
     }
 }
